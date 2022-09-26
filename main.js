86dd80844eadc3cf246d2a9ea617cd0646cbc8d8
GaoxiangZhang09 <105685880+GaoxiangZhang09@users.noreply.github.com>
2022-09-26 12:42:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// const myHeading = document.querySelector('');
+// myHeading.textContent = 'Hello world!';
+
+// It is design for show/hide the bar when screen is under certion px
+var navLinks = document.getElementById("naveLinks");
+function showMenu(){
+    naveLink.style.right = "0";
+}
+
+function hideMenu(){
+    naveLink.style.right = "-200px";
+}
